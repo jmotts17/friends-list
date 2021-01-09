16636e04ca514b126c4ef569bf81ce46dce5b405
Josh Motta <joshua.a.motta@gmail.com>
2021-01-09 10:03:34 -0500

    Added bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'friends-list';
+  newFriend = '';
+  isDisabled = false;
   // create an array named friends and populate with some friends
   friends:string[] = ["Sean", "Ron", "Julie", "Phil"];
+
+  addFriend() {
+    this.friends.push(this.newFriend);
+  }
 }
